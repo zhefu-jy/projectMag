9ab115e4e0be9b68e4eb4bb8a3f55afd7edd950d
zhefu-jy <1584504718@qq.com>
2022-05-20 09:27:28 +0800

    模拟代码冲突本地

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("项目管理提交测试");
         System.out.println("模拟代码冲突local");
+        System.out.println("模拟代码冲突local");
     }
 }
